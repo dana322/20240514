775e435ca497dd30aefb849dccbca26a9ae1f772
dana322 <sbzddanan@gmail.com>
2025-05-15 00:41:04 +0800

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,15 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const metadata = {
+  title: "PDF页面旋转工具 - 在线免费旋转PDF",
+  description:
+    "简单易用的在线PDF页面旋转工具，支持单页、多页旋转，免费保存，无需注册。",
+  other: {
+    "google-site-verification": "TFep6kGBflKC0tna2TM26LJeEVE2gtTIa7ahd3v2cA8",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,21 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zn">
-      <Head>
-        <title>PDF页面旋转工具 - 在线免费旋转PDF</title>
-        <meta
-          name="google-site-verification"
-          content="TFep6kGBflKC0tna2TM26LJeEVE2gtTIa7ahd3v2cA8"
-        />
-        <meta
-          name="description"
-          content="简单易用的在线PDF页面旋转工具，支持单页、多页旋转，免费保存，无需注册。"
-        />
-        <meta
-          name="keywords"
-          content="PDF, PDF旋转, 在线工具, 免费, 页面旋转, Rotate PDF"
-        />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
